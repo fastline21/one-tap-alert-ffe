6c047bb0c4e06c795e12d4aaf3a1a276f871483a
Joshwa Facistol <fastline2123@gmail.com>
2022-08-28 23:36:34 +0800

    Init server

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const session = require('express-session');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json({ extended: true }));
+
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true,
+	})
+);
+
+app.get('/', (req, res) => {
+	res.send('Welcome to One-Tap Alert FFE');
+});
+
+// app.use('/api', require('./routes'));
+
+const port = process.env.PORT || 5000;
+const hostname = process.env.HOSTNAME || 'localhost';
+
+app.listen(port, () =>
+	console.log(`Server running on http://${hostname}:${port}`)
+);
