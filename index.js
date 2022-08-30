762d886e2e90c219f260a9c2afb203cb4b598ae8
Joshua Onio <oniojoshua1900@gmail.com>
2022-08-30 10:09:24 +0800

    remove client

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const session = require("express-session");
 
 const db = require("./config/db");
@@ -23,15 +22,6 @@ app.use(
 );
 
 app.use("/api", require("./routes"));
-
-if (process.env.NODE_ENV === "production") {
-	// Set static folder
-	app.use(express.static("client/build"));
-
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-	);
-}
 
 const port = process.env.PORT || 5000;
 
