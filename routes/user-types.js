d0b96611591b89a4dfc30721a81aab18ded6e50a
Joshua Onio <oniojoshua1900@gmail.com>
2022-09-03 01:20:42 +0800

    Remove date fields in user-types route

--- routes/user-types.js
+++ routes/user-types.js
@@ -8,7 +8,9 @@ const auth = require('./../middleware/auth');
 const UserTypes = require('./../models/user_types');
 
 router.get('/', routeAuth, auth, async (req, res) => {
-  const result = await UserTypes.find();
+  const result = await UserTypes.find().select(
+    '-date_added -date_modified -date_deleted'
+  );
 
   res.json({ data: { user_types: result }, status_code: 200 });
 });
