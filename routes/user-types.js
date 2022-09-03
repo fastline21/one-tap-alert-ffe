dc9f3e19d4c95bcb2fc6b18362911add0b812004
Joshua Onio <oniojoshua1900@gmail.com>
2022-09-03 17:15:48 +0800

    Add specific user type route

--- routes/user-types.js
+++ routes/user-types.js
@@ -7,12 +7,30 @@ const auth = require('./../middleware/auth');
 
 const UserTypes = require('./../models/user_types');
 
+// Get all user types
 router.get('/', routeAuth, auth, async (req, res) => {
   const result = await UserTypes.find().select(
     '-date_added -date_modified -date_deleted'
   );
 
-  res.json({ data: { user_types: result }, status_code: 200 });
+  res.status(200).json({ data: { user_types: result }, status_code: 200 });
 });
+
+// Get user type
+router.get('/:id', routeAuth, auth, async (req, res) => {
+  const { id } = req.params;
+
+  const result = await UserTypes.findById(id).select(
+    '-date_added -date_modified -date_deleted'
+  );
+
+  res.status(200).json({ data: { user_type: result }, status_code: 200 });
+});
+
+// Create new user type
+
+// Update user type
+
+// Delete user type
 
 module.exports = router;
