1a950d5b0e041405a89a57bbd2040b47b73bbe88
Joshwa Facistol <fastline2123@gmail.com>
2022-09-19 15:28:54 +0800

    Applied root dir to client build script

--- server/index.js
+++ server/index.js
@@ -26,7 +26,8 @@ app.use('/api', require('./routes'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  const clientDir = path.resolve('client', 'build');
+  const clientDir = path.resolve('./', 'client', 'build');
+  console.log(clientDir);
   // Set static folder
   app.use(express.static(clientDir));
 
