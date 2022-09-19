e72713a852c3d6bac2e1256c3edf98aee46d4332
Joshwa Facistol <fastline2123@gmail.com>
2022-09-19 14:25:57 +0800

    Added path

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const session = require('cookie-session');
+const path = require('path');
 
 const db = require('./config/db');
 db();
