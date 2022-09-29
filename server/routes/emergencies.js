be1c44875128127b489353aa93c5f3ad6d97bb7a
Joshwa Facistol <fastline2123@gmail.com>
2022-09-29 17:40:27 +0800

    Added export router emergencies route

--- server/routes/emergencies.js
+++ server/routes/emergencies.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const Emergencies = require("./../models/emergencies");
+const Emergencies = require('./../models/emergencies');
 
-const routeAuth = require("./../middleware/route-auth");
-const auth = require("./../middleware/auth");
+const routeAuth = require('./../middleware/route-auth');
+const auth = require('./../middleware/auth');
 
-router.post("/", routeAuth, auth, (req, res) => {
-	const { emergency_type_id, longitude, latitude } = req.body;
-	const { id: user_id } = req.user;
+router.post('/', routeAuth, auth, (req, res) => {
+  const { emergency_type_id, longitude, latitude } = req.body;
+  const { id: user_id } = req.user;
 
-	res.json({ user_id, emergency_type_id, longitude, latitude });
+  res.json({ user_id, emergency_type_id, longitude, latitude });
 });
+
+module.exports = router;
