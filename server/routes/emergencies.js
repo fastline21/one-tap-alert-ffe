2bcc0e881a7cd509f1802aa97ee5b7f04c33b30f
Joshwa Facistol <fastline2123@gmail.com>
2022-10-04 15:16:44 +0800

    Updated Create Emergency endpoint

--- server/routes/emergencies.js
+++ server/routes/emergencies.js
@@ -5,34 +5,61 @@ const fs = require('fs');
 
 const router = express.Router();
 
-const Emergencies = require('./../models/emergencies');
+// Models
+const EmergenciesModel = require('./../models/emergencies');
+const EmergencyStatusesModel = require('../models/emergency_statuses');
 
+// Middleware
 const routeAuth = require('./../middleware/route-auth');
 const auth = require('./../middleware/auth');
 
-router.post('/', routeAuth, auth, (req, res) => {
-  const { emergency_type_id, longitude, latitude } = req.body;
-  const { id: user_id } = req.user;
-  const { captured_image_uri } = req.files;
+/**
+ * Create Emergency
+ */
+router.post('/', routeAuth, auth, async (req, res) => {
+  const { emergency_type_id: emergencyTypeID, latitude, longitude } = req.body;
+  const { id: userID } = req.user;
+  const { captured_image_uri: capturedImageURI } = req.files;
 
-  // Log Capture Image URI
-  console.log(captured_image_uri);
+  try {
+    const uploadPathDir = path.join('./', 'server', 'captured-image');
 
-  const uploadPathDir = path.join('./', 'server', 'captured-image');
+    if (!fs.existsSync(uploadPathDir)) {
+      fs.mkdirSync(uploadPathDir, { recursive: true });
+    }
 
-  if (!fs.existsSync(uploadPathDir)) {
-    fs.mkdirSync(uploadPathDir, { recursive: true });
+    const filename = uuidv4();
+    const fileExt = path.extname(capturedImageURI.name);
+    const file = `${filename}${fileExt}`;
+
+    const uploadPath = path.resolve(uploadPathDir, file);
+
+    capturedImageURI.mv(uploadPath);
+
+    const PendingEmergencyStatus = await EmergencyStatusesModel.findOne({
+      name: 'Pending',
+    });
+
+    const newEmergency = new EmergenciesModel({
+      user_id: userID,
+      emergency_type_id: emergencyTypeID,
+      emergency_status_id: PendingEmergencyStatus._id,
+      latitude,
+      longitude,
+    });
+
+    await newEmergency.save();
+
+    return res.status(200).json({
+      data: { success: true, message: 'You successfully submit an emergency' },
+      status_code: 200,
+    });
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    return res
+      .status(500)
+      .json({ data: { message: 'Server Error' }, status_code: 500 });
   }
-
-  const filename = uuidv4();
-  const fileExt = path.extname(captured_image_uri.name);
-  const file = `${filename}${fileExt}`;
-
-  const uploadPath = path.resolve(uploadPathDir, file);
-
-  captured_image_uri.mv(uploadPath);
-
-  res.json({ data: { success: true }, status_code: 200 });
 });
 
 module.exports = router;
